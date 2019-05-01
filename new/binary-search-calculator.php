10f2420b9c14e9555f284ac03040228bc05679b5
doveway <25659598+doveway@users.noreply.github.com>
2019-05-01 15:21:10 +0100

    Add files via upload

---
+++ new/binary-search-calculator.php
@@ -0,0 +1,108 @@
+<?php 
+  error_reporting(0 & ~E_DEPRECATED);
+  
+//$dividend = -6; 
+//$divisor = 5;  
+$dividend = $_POST['dividend'];
+$divisor = $_POST['divisor'];
+ 
+
+
+
+?> 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Binary Search Calculator</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="css/util.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+<!--===============================================================================================-->
+</head>
+
+    <body>
+    <div class="limiter">
+		<div class="container-login100" style="background-image: url('images/bg-01.jpg');">
+			<div class="wrap-login100 p-t-30 p-b-50">
+				<span class="login100-form-title p-b-41">
+					Binary Search Calculator
+                </span>
+                    <?php 
+                    function mod($dividend, $divisor) {
+                        return ($dividend % $divisor) + ($dividend < 0 ? $divisor : 0);
+					}
+					
+					if(isset($_POST['dividend'])){ 
+						if ($divisor > 0 || $divisor< 0){
+                        echo // floor(intdiv($dividend, $divisor)) ."<br>".  
+                        "<span class='login100-form-title p-b-41'>Result: " . floor($dividend/ $divisor) ."</span>"; 
+                    
+                        $remainder = mod($dividend, $divisor);
+                      echo "<span class='login100-form-title p-b-41'>Remainder:" . $remainder. "</span>";
+                    }else {
+						
+						echo "<span class='login100-form-title p-b-41'>Sorry 0(zero) is not allowed as divisor</span>";
+						
+                    }
+                    
+					
+				}// end of isset()
+                    ?>
+                    <form class="login100-form validate-form p-b-33 p-t-5"  action='' method='POST'>
+                    <div class="wrap-input100 validate-input" data-validate = "Enter dividend">
+						<input class="input100" type="number" name="dividend" placeholder="Dividend" required>
+						<span class="focus-input100" ></span>
+					</div>
+
+					<div class="wrap-input100 validate-input" data-validate="Enter divisor">
+						<input class="input100" type="number" name="divisor" placeholder="Divisor" required>
+						<span class="focus-input100" ></span>
+					</div>
+
+					<div class="container-login100-form-btn m-t-32">
+						<button class="login100-form-btn">
+							Calculate
+						</button>
+					</div>
+                </form>
+
+                <div id="dropDownSelect1"></div>
+<!--===============================================================================================-->
+	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/animsition/js/animsition.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/bootstrap/js/popper.js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/daterangepicker/moment.min.js"></script>
+	<script src="vendor/daterangepicker/daterangepicker.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/countdowntime/countdowntime.js"></script>
+<!--===============================================================================================-->
+    <script src="js/main.js"></script>
+    
+    </body>
+    
+</html>
